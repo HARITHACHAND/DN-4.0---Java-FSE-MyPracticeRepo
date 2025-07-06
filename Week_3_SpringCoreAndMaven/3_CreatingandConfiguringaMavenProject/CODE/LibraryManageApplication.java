3edd74c22a242919bdd9e67135fc3db563047494
HARITHACHAND <119292407+HARITHACHAND@users.noreply.github.com>
2025-07-06 22:14:26 +0530

    Update LibraryManageApplication.java

--- Week_3_SpringCoreAndMaven/3_CreatingandConfiguringaMavenProject/CODE/LibraryManageApplication.java
+++ Week_3_SpringCoreAndMaven/3_CreatingandConfiguringaMavenProject/CODE/LibraryManageApplication.java
@@ -1,16 +1,4 @@
-//package com.example.LibraryManagemnet;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class LibraryManagemnetApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(LibraryManagemnetApplication.class, args);
-//	}
-//
-//}
+
 package com.example;
 
 import org.springframework.boot.SpringApplication;
